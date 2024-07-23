4600c0a32175521242e9ac28defd47ba9cd8c351
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-07-23 22:04:39 +0700

    [C7 FE] Dashboard Car List React

--- client/src/components/dashboard/cars/carCard.tsx
+++ client/src/components/dashboard/cars/carCard.tsx
@@ -1,6 +1,12 @@
 import { Car } from '../../../context/carContext';
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import '../../../style/dashboard/style.css'
+
+import keyIcon from '../../../assets/icons/dashboard/fi_key.png';
+import clockIcon from '../../../assets/icons/dashboard/fi_clock.png';
+import trashIcon from '../../../assets/icons/dashboard/fi_trash-2.png';
+import editIcon from '../../../assets/icons/dashboard/fi_edit.png';
 
 interface CarItemProps {
     car: Car;
@@ -9,27 +15,32 @@ interface CarItemProps {
 export default function CarItem({ car }: CarItemProps): JSX.Element {
     return (
         <section className="cars-card card px-0">
-            <img src={car.image} className="cars-card-img object-cover" alt={car.manufacture} />
+            <img src={car.image}
+                className="cars-card-img" alt={car.manufacture} />
             <section className="cars__card-body card-body">
                 <h5 className="cars__name">{car.manufacture}/{car.model}</h5>
                 <h3 className="cars__price">Rp {car.rentPerDay} / hari</h3>
-                <p className="cars__description">{car.description}</p>
-                <section className="car__spec grid grid-cols-3 gap-4">
-                    <section className="cars__spec flex items-center">
-                        <img className="cars__spec-icon" src="./assets/icons/cars/fi_users.png" alt="" />
-                        <p className="car__spec-text">{car.capacity} orang</p>
+                <section className="car__spec">
+                    <section className="cars__spec d-flex align-self-center">
+                        <img className="cars__spec-icon" src={keyIcon} alt="" />
+                        <p className="car__spec-text">Start rent - Finish rent</p>
                     </section>
-                    <section className="cars__spec flex items-center">
-                        <img className="cars__spec-icon" src="./assets/icons/cars/fi_settings.png" alt="" />
-                        <p className="car__spec-text">{car.transmission}</p>
-                    </section>
-                    <section className="cars__spec flex items-center">
-                        <img className="cars__spec-icon" src="./assets/icons/cars/fi_calendar.png" alt="" />
-                        <p className="car__spec-text">Tahun {car.year}</p>
+                    <section className="cars__spec d-flex align-self-center">
+                        <img className="cars__spec-icon" src={clockIcon} alt="" />
+                        <p className="car__spec-text">Updated at {car.updatedAt}</p>
                     </section>
                 </section>
             </section>
-            <button className="btn cars__button">Pilih Mobil</button>
+            <section className="d-flex">
+                <button className="btn cars__button delete-button d-flex justify-content-center">
+                    <img src={trashIcon} alt="" />
+                    <p>Delete</p>
+                </button>
+                <button className="btn cars__button edit-button d-flex justify-content-center">
+                    <img src={editIcon} alt="" />
+                    <p>Edit</p>
+                </button>
+            </section>
         </section>
     );
 }
---
+++ client/src/components/dashboard/cars/carsListSection.tsx
@@ -0,0 +1,75 @@
+import { useEffect } from 'react';
+import { useCarContext } from '../../../context/carContext';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../../../style/dashboard/style.css'
+
+import CarItem from './carCard';
+
+import menuIcon from '../../../assets/icons/dashboard/fi_menu.png';
+import userPPIcon from '../../../assets/icons/dashboard/user_pp.png';
+import chevronDownIcon from '../../../assets/icons/dashboard/fi_chevron-down.png';
+
+export default function CarsListSection(): JSX.Element {
+    const { cars, fetchCars } = useCarContext();
+
+    useEffect(() => {
+        fetchCars();
+    }, [fetchCars]);
+
+    return (
+        <section className="col-9 section-car-form" style={{ paddingLeft: 0 }}>
+            <section className="top-navbar d-flex justify-content-between">
+                <img src={menuIcon} className="top-navbar__hamburger" alt="" />
+                <section className="d-flex">
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <section className="d-flex align-items-center" style={{ marginLeft: '24px' }}>
+                        <img src={userPPIcon} className="top-navbar__user-picture" alt="" />
+                        <p className="top-navbar__username">RazinSyakib</p>
+                        <img src={chevronDownIcon} className="top-navbar__dropdown"
+                            alt="" />
+                    </section>
+                </section>
+            </section>
+            <section className="list-car-section">
+                <section className="car-form__breadcrumb">
+                    <nav aria-label="breadcrumb">
+                        {/* style="--bs-breadcrumb-sectionider: '>';" */}
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item car-form__breadcrumb-item"><a href="/dashboard/cars"
+                                style={{ color: 'black' }}>Cars</a></li>
+                            <li className="breadcrumb-item active" aria-current="page">List Car</li>
+                        </ol>
+                    </nav>
+                </section>
+                <section className="d-flex justify-content-between">
+                    <h2 className="list-car-section__title">List Car</h2>
+                    <a href="/dashboard/car/add-car">
+                        <button type="button" className="btn btn-primary add-new-car-button"><span
+                            style={{ marginRight: '12px' }}>+</span>Add
+                            New Car</button>
+                    </a>
+                </section>
+
+                <section className=" d-flex" style={{ marginBottom: '24px' }}>
+                    <button type="button"
+                        className="btn btn-primary list-car-section__filter-button list-car-section__filter-button-active">All</button>
+                    <button type="button" className="btn btn-primary list-car-section__filter-button">Small</button>
+                    <button type="button" className="btn btn-primary list-car-section__filter-button">Medium</button>
+                    <button type="button" className="btn btn-primary list-car-section__filter-button">Large</button>
+                </section>
+
+                <section className="row row-cols-3">
+                    {cars.map(car => (
+                        <section className="col mb-4" key={car.id}>
+                            <CarItem car={car} />
+                        </section>
+                    ))}
+                </section>
+            </section>
+        </section>
+    );
+}
