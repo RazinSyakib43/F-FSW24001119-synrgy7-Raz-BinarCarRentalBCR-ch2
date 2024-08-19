ee620a61efa7bf0db1bec8be41247bc25c140917
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-20 04:14:06 +0700

    [C7 FE] Styling Login Page

--- client/src/pages/auth/login.tsx
+++ client/src/pages/auth/login.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { useAuth } from '../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 
+import bannerImage from '../../assets/images/login/loginImage.png';
+import loginLogo from '../../assets/images/login/loginLogo.png';
+
+import '../../style/login/style.css';
+
 export function LoginPage() {
     const { login } = useAuth();
     const [email, setEmail] = useState('');
@@ -19,16 +24,29 @@ export function LoginPage() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Email:
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <button type="submit">Login</button>
-        </form>
+        <section className="loginPage">
+            <div className="bannerImage">
+                <img src={bannerImage} alt="" />
+            </div>
+            <div className="loginForm">
+                <img className="loginLogo" src={loginLogo} alt="" />
+                <div className="loginForm__header">
+                    <h1 className="login_welcome_title">Welcome, Admin BCR</h1>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group
+                    ">
+                        <label className="loginLabel" htmlFor="email">Email</label>
+                        <input type="email" className="form-control loginInput" id="email" aria-describedby="emailHelp"
+                            placeholder="Contoh: johndee@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label className="loginLabel" htmlFor="password">Password</label>
+                        <input type="password" className="form-control loginInput" id="password" placeholder="6+ karakter" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button type="submit" className="btn loginButton mt-3">Sign In</button>
+                </form>
+            </div>
+        </section>
     );
 }
