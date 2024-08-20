390866d93aa4c209c075cc0368aae71672a517c6
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-21 03:43:14 +0700

    [C8 FE*] Remove Server project

--- server/middleware/multerMemory.ts
+++
@@ -1,5 +0,0 @@
-import multer from 'multer';
-
-const storage = multer.memoryStorage(); // menyimpan file sementara di memory storage (RAM), sehingga tidak perlu menyimpan file di disk storage
-
-export default multer({ storage })
--- server/utils/randomString.ts
+++
@@ -1,9 +0,0 @@
-// import crypto from 'crypto';
-
-// export default function randomString(length: number): string {
-//   return crypto.randomBytes(32).toString('hex').slice(0, length);
-// }
-
-// console.log(randomString); // Output: 3e0b1e
-
-// node - e "console.log(require('crypto').randomBytes(32).toString('hex'))"   
